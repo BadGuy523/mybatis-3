42576aab2962a43067e07ee687618c3cfa92afbe
BadGuy523 <694255828@qq.com>
2021-02-07 22:29:07 +0800

    mybatis源码准备

--- src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
+++ src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
@@ -15,13 +15,6 @@
  */
 package org.apache.ibatis.autoconstructor;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.io.Reader;
-import java.util.List;
-
-import org.apache.ibatis.BaseDataTest;
 import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -31,26 +24,50 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.Reader;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class AutoConstructorTest {
   private static SqlSessionFactory sqlSessionFactory;
 
   @BeforeAll
   static void setUp() throws Exception {
     // create a SqlSessionFactory
+    // 读取mybatis配置文件
     try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/mybatis-config.xml")) {
       sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
     }
 
     // populate in-memory database
-    BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
-        "org/apache/ibatis/autoconstructor/CreateDB.sql");
+    // 将内置的sql插入所连数据库(首次调试可运行，之后可注释)
+    // TODO： 存在问题，每调用一次，没有按照sql语句中的先drop再create再insert，而是直接insert导致数据重复
+//    BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
+//        "org/apache/ibatis/autoconstructor/CreateDB.sql");
   }
 
+  /**
+   * 测试setUp静态初始化方法
+   */
+  @Test
+  void init() {
+    System.out.println("init config...");
+  }
+
+  /**
+   * 简单测试查询操作
+   */
   @Test
   void fullyPopulatedSubject() {
+    // 通过sqlSession工厂类获取sqlSession连接
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      // 通过sqlSession获取对应的mapper接口
+      // TODO:此处用到了基于jdk动态代理演变的动态代理,无具体实现类,待跟踪分析
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
       final Object subject = mapper.getSubject(1);
+      // 非空断言,subject为空则抛出异常
       assertNotNull(subject);
     }
   }
